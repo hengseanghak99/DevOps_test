c29412a9a2def489b78f22b5597e6a649390428f
hengseanghak99 <hengseanghak10499@gmail.com>
2025-06-19 14:56:31 +0700

    test build with docker

---
+++ tests/example1.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.selenium.dev/selenium/web/web-form.html');
+  await page.getByRole('textbox', { name: 'Text input' }).click();
+  await page.getByRole('textbox', { name: 'Text input' }).fill('test');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('12233456');
+  await page.getByRole('textbox', { name: 'Textarea' }).click();
+  await page.getByRole('textbox', { name: 'Textarea' }).fill('terster');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(page.getByText('Received!')).toBeVisible();
+});
